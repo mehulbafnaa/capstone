84b16290d5896f7a04e0ee2d2390976552959864
Mehul Bafna <mbafna@mail.yu.edu>
2025-07-07 06:45:18 +0000

    revreting mistakes

--- finetuning/train.py
+++ finetuning/train.py
@@ -59,7 +59,7 @@ def train_step(state, batch, dropout_rng):
         logits = state.apply_fn(
             {"params": params},
             tokens = batch["input_ids"],
-            attention_mask=batch["attention_mask"],
+            # attention_mask=batch["attention_mask"],
             segment_pos=batch["segment_pos"],
             rngs={"dropout": new_dropout_rng}
         )[0]
