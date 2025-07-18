72206de18d248d6f25f849671418336f8d542831
Mehul Bafna <mbafna@mail.yu.edu>
2025-07-18 03:04:54 +0000

    finetuning

--- 2b-it/download_model.sh
+++ 2b-it/download_model.sh
@@ -7,8 +7,3 @@ curl -L -u $KAGGLE_USERNAME:$KAGGLE_KEY\
   -o ~/2b-it/model.tar.gz\
   https://www.kaggle.com/api/v1/models/google/recurrentgemma/flax/2b-it/1/download
 
-
-
-  export KAGGLE_USERNAME=mehulbafnaa
-
-  export KAGGLE_KEY=450080c1c9fbe22c908798f4f05bf9c4
