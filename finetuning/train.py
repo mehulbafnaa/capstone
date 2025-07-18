f34f286f1975cac74a94dd236adc49d03d01523d
Mehul Bafna <mbafna@mail.yu.edu>
2025-07-18 04:37:11 +0000

    new sharding approach

--- finetuning/train.py
+++ finetuning/train.py
@@ -958,10 +958,9 @@ def main(argv):
 
     num_hosts = jax.process_count()
     devices_per_host = jax.local_device_count()
-    devices_array = jax.devices().reshape((num_hosts, devices_per_host))
+    # devices_array = jax.devices().reshape((num_hosts, devices_per_host))
+    devices_array = np.array(jax.devices()).reshape((num_hosts, devices_per_host))
     mesh = Mesh(devices_array, (config.data_axis, config.model_axis))
-
-    mesh = Mesh(jax.devices(), (config.data_axis, config.model_axis))
     
     logging.set_verbosity(logging.INFO)
     if jax.process_index() == 0:
